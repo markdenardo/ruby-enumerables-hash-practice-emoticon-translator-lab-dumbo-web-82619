13a997c734bd2a38dc01c4e9b17483b9729cb7da
Mark DeNardo <mark.denardo@gmail.com>
2019-08-22 21:05:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,26 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  hash = {:get_emoticon=>{}, :get_meaning=>{}}
+  results = YAML.load_file(file_path)
+    results.each do |string, emos|
+      #binding.pry
+#write in to our hash a new key valued pair of the string and the emoticon
+    hash[:get_emoticon][string] = new_val
+    # hash[:get_emoticon][emos(0)]
+    # hash[:get_meaning][emos(1)]
+    end
+  hash
+ #Write a method, load_library, that loads the emoticons.yml file.
 end
 
 def get_japanese_emoticon
-  # code goes here
+
+ #Write a method, get_japanese_emoticon, that will take a traditional Western emoticon, like :) and translate it to its Japanese version. It will rely load_library to work. Refer to the table below for translations.
 end
 
 def get_english_meaning
-  # code goes here
+  #Write a method, get_english_meaning, that takes a Japanese emoticon and returns its meaning in English. This method will also rely on load_library to first load the YAML file.
 end
