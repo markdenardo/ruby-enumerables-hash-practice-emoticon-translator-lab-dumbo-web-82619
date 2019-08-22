ef21b7273c99678d7fb732fba59baded2e3f2daf
Mark DeNardo <mark.denardo@gmail.com>
2019-08-22 21:38:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_japanese_emoticon(path, e_emoticon)
   trans = load_library(path)
-  trans[:get_emoticon]
+  trans[:get_emoticon][e_emoticon]
 
 
  #Write a method, get_japanese_emoticon, that will take a traditional Western emoticon, like :) and translate it to its Japanese version. It will rely load_library to work. Refer to the table below for translations.
